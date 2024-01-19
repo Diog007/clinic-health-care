7e3c5975ec11c3d8384377d7d0b4dd13d4f88f82
Diog007 <diogo.mz123@gmail.com>
2024-01-19 18:12:08 -0300

    added JPA annotations for domain classes

--- src/main/java/consul/med/api/medico/Medico.java
+++ src/main/java/consul/med/api/medico/Medico.java
@@ -1,23 +1,37 @@
 package consul.med.api.medico;
 
 import consul.med.api.endereco.Endereco;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+
+@Table(name = "medicos")
+@Entity(name = "Medico")
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
 public class Medico {
 
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String email;
     private String crm;
 
+    @Enumerated(EnumType.STRING)
     private Especialidade especialidade;
 
+    @Embedded
     private Endereco endereco;
 }
