d61529c06af218e4c7104e3444a1b82365381db6
Diog007 <diogo.mz123@gmail.com>
2024-01-19 22:21:30 -0300

    Create method register in the Controller class

--- src/main/java/consul/med/api/controller/MedicoController.java
+++ src/main/java/consul/med/api/controller/MedicoController.java
@@ -1,19 +1,26 @@
 package consul.med.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import consul.med.api.medico.DadosCadastroMedico;
+import consul.med.api.medico.Medico;
+import consul.med.api.medico.MedicoRepository;
 
 @RestController
 @RequestMapping("medicos")
 public class MedicoController {
-
+	
+	@Autowired
+	private MedicoRepository repository;
+	
+	
 	@PostMapping
 	public void cadastrar(@RequestBody DadosCadastroMedico dados) {
-		System.out.println(dados);
+		repository.save(new Medico(dados));
 	}
 	
 }
