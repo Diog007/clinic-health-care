c4394af6af14407b0bca425c686757b0a9f912d4
Diog007 <diogo.mz123@gmail.com>
2024-01-21 10:37:34 -0300

    Create method to exclude physical doctor

--- src/main/java/consul/med/api/controller/MedicoController.java
+++ src/main/java/consul/med/api/controller/MedicoController.java
@@ -5,7 +5,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -44,4 +46,11 @@ public class MedicoController {
 		var medico = repository.getReferenceById(dados.id());
 		medico.atualizarInformacoes(dados);
 	}
+	
+	@DeleteMapping("/{id}")
+	@Transactional
+	public void excluirMedico(@PathVariable Long id) {
+		repository.deleteById(id);
+	}
+	
 }
