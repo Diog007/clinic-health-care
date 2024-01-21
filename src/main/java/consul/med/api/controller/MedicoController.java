92ae2d46706b475a8451d9336e360dc345192b6f
Diog007 <diogo.mz123@gmail.com>
2024-01-21 10:16:04 -0300

    create method update Doctor data

--- src/main/java/consul/med/api/controller/MedicoController.java
+++ src/main/java/consul/med/api/controller/MedicoController.java
@@ -3,13 +3,16 @@ package consul.med.api.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import consul.med.api.medico.DadosAtualizacaoMedico;
 import consul.med.api.medico.DadosCadastroMedico;
 import consul.med.api.medico.DadosListagemMedico;
 import consul.med.api.medico.Medico;
@@ -26,13 +29,19 @@ public class MedicoController {
 	
 	@PostMapping
 	@Transactional
-	public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
+	public void cadastrarMedico(@RequestBody @Valid DadosCadastroMedico dados) {
 		repository.save(new Medico(dados));
 	}
 	
 	@GetMapping
-	public Page<DadosListagemMedico> listarMedico(Pageable paginacao){
+	public Page<DadosListagemMedico> listarMedico(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao){
 		return repository.findAll(paginacao).map(DadosListagemMedico :: new);
 	}
 	
+	@PutMapping
+	@Transactional
+	public void atualizarMedico(@RequestBody @Valid DadosAtualizacaoMedico dados) {
+		var medico = repository.getReferenceById(dados.id());
+		medico.atualizarInformacoes(dados);
+	}
 }
---
+++ src/main/java/consul/med/api/medico/DadosAtualizacaoMedico.java
@@ -0,0 +1,13 @@
+package consul.med.api.medico;
+
+import consul.med.api.endereco.DadosEndereco;
+import jakarta.validation.constraints.NotNull;
+
+public record DadosAtualizacaoMedico(
+		@NotNull
+		Long id,
+		String nome,
+		String telefone,
+		DadosEndereco endereco) {
+
+}
