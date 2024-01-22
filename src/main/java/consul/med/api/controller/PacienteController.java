46d69992bd041062eb6dadf24a1bd84dbdee1fd8
Diog007 <diogo.mz123@gmail.com>
2024-01-22 11:17:59 -0300

    Create Register Paciente method

--- src/main/java/consul/med/api/controller/PacienteController.java
+++ src/main/java/consul/med/api/controller/PacienteController.java
@@ -1,9 +1,13 @@
 package consul.med.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import consul.med.api.paciente.DadosCadastroPaciente;
+import consul.med.api.paciente.Paciente;
 import consul.med.api.paciente.PacienteRepository;
 
 @RestController
@@ -13,4 +17,10 @@ public class PacienteController {
 	@Autowired
 	private PacienteRepository repository;
 	
+	@PostMapping
+	public void cadastrarPaciente (@RequestBody DadosCadastroPaciente dados) {
+		repository.save(new Paciente(dados));
+		
+		
+	}
 }
