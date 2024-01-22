c27738ff31d67a5b89a5c15b32e37f69742f47f2
Diog007 <diogo.mz123@gmail.com>
2024-01-22 12:06:04 -0300

    Create Paciente Listing method

--- src/main/java/consul/med/api/controller/PacienteController.java
+++ src/main/java/consul/med/api/controller/PacienteController.java
@@ -1,13 +1,20 @@
 package consul.med.api.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import consul.med.api.paciente.DadosCadastroPaciente;
+import consul.med.api.paciente.DadosListagemPaciente;
 import consul.med.api.paciente.Paciente;
 import consul.med.api.paciente.PacienteRepository;
 import jakarta.validation.Valid;
@@ -23,5 +30,10 @@ public class PacienteController {
 	@Transactional
 	public void cadastrarPaciente (@RequestBody @Valid DadosCadastroPaciente dados) {
 		repository.save(new Paciente(dados));
+	}
+	
+	@GetMapping
+	public Page<DadosListagemPaciente> listaPaciente(@PageableDefault(size = 10, sort = {"nome"})Pageable paginacao ){
+		return repository.findAll(paginacao).map(DadosListagemPaciente :: new);
 	}
 }
---
+++ src/main/java/consul/med/api/paciente/DadosListagemPaciente.java
@@ -0,0 +1,12 @@
+package consul.med.api.paciente;
+
+public record DadosListagemPaciente(
+		Long id,
+		String nome,
+		String email,
+		String cpf) {
+	
+		public DadosListagemPaciente (Paciente paciente) {
+			this(paciente.getId(), paciente.getNome(), paciente.getEmail(), paciente.getCpf());
+		}
+}
