d1bac796c6f56b44dba083e01fd22d3546f88ace
Diog007 <diogo.mz123@gmail.com>
2024-01-22 13:45:57 -0300

    Create Delete Paciente method

--- src/main/java/consul/med/api/medico/Medico.java
+++ src/main/java/consul/med/api/medico/Medico.java
@@ -56,9 +56,6 @@ public class Medico {
 		if(dados.telefone() != null) {
 			this.telefone = dados.telefone();
 		}
-		if(dados.telefone() != null) {
-			this.telefone = dados.telefone();
-		}
 		if(dados.endereco() != null) {
 			this.endereco.atualizarInformacoes(dados.endereco());
 		}
