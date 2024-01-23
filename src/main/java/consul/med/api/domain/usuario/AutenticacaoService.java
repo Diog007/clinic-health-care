d8a9809e940997c2961587349b8b931f0af28afe
Diog007 <diogo.mz123@gmail.com>
2024-01-23 15:20:33 -0300

    Login authentication method implemented

---
+++ src/main/java/consul/med/api/domain/usuario/AutenticacaoService.java
@@ -0,0 +1,17 @@
+package consul.med.api.domain.usuario;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AutenticacaoService implements UserDetailsService{
+
+	private UsuarioRepository repository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		return repository.findByLogin(username);
+	}
+}
