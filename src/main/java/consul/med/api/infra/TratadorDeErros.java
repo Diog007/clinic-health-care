210c91ab37b9f48016a2b7da6f934040c2ea963e
Diog007 <diogo.mz123@gmail.com>
2024-01-22 21:15:15 -0300

    Implementing 400 error handling

--- src/main/java/consul/med/api/infra/TratadorDeErros.java
+++ src/main/java/consul/med/api/infra/TratadorDeErros.java
@@ -1,6 +1,8 @@
 package consul.med.api.infra;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -13,5 +15,20 @@ public class TratadorDeErros {
 	public ResponseEntity tratarErro404() {
 		return ResponseEntity.notFound().build();
 	}
-
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity tratarError400(MethodArgumentNotValidException ex) {
+		var erros = ex.getFieldErrors();
+		return ResponseEntity.badRequest().body(erros.stream().map(DadosErroValidacao::new).toList());	
+		}
+	
+	
+	private record DadosErroValidacao (String campo, String mensagem) {
+		
+		
+		public DadosErroValidacao(FieldError erro) {
+			this(erro.getField(), erro.getDefaultMessage());
+		}
+		
+	}
 }
