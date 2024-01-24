ef5b5b260575c3ea13e6e7c022bb405b28867d1e
Diog007 <diogo.mz123@gmail.com>
2024-01-24 15:00:43 -0300

    Adjustment in the generation of JWT TOken

--- src/main/java/consul/med/api/controller/AutenticacaoController.java
+++ src/main/java/consul/med/api/controller/AutenticacaoController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import consul.med.api.domain.usuario.DadosAutenticacao;
 import consul.med.api.domain.usuario.Usuario;
+import consul.med.api.infra.security.DadosTokenJWT;
 import consul.med.api.infra.security.TokenService;
 import jakarta.validation.Valid;
 
@@ -26,10 +27,12 @@ public class AutenticacaoController {
 	
 	@PostMapping
 	public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dados) {
-		var token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
-		var authentication = manager.authenticate(token);
+		var authenticationToken = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
+		var authentication = manager.authenticate(authenticationToken);
 		
-		return ResponseEntity.ok(tokenService.gerarToken((Usuario) authentication.getPrincipal()));
+		var tokenJWT = tokenService.gerarToken((Usuario) authentication.getPrincipal());
+		
+		return ResponseEntity.ok(new DadosTokenJWT(tokenJWT));
 	}
 
 }
---
+++ src/main/java/consul/med/api/infra/security/DadosTokenJWT.java
@@ -0,0 +1,5 @@
+package consul.med.api.infra.security;
+
+public record DadosTokenJWT(String token) {
+
+}
