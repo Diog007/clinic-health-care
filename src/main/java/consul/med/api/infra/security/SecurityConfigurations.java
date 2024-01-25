f9af4649450c92644f47f91946c36f10a228e563
Diog007 <diogo.mz123@gmail.com>
2024-01-25 15:21:55 -0300

    Authenticate the user

--- src/main/java/consul/med/api/infra/security/SecurityConfigurations.java
+++ src/main/java/consul/med/api/infra/security/SecurityConfigurations.java
@@ -2,6 +2,7 @@ package consul.med.api.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -18,18 +19,21 @@ public class SecurityConfigurations {
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		return http.csrf(csrf -> csrf.disable())
-				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-				.and().build();
+				.sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.authorizeHttpRequests(req -> {
+					req.requestMatchers(HttpMethod.POST, "/login").permitAll();
+					req.anyRequest().authenticated();
+				}).build();
 	}
-	
+
 	@Bean
 	public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
 		return configuration.getAuthenticationManager();
 	}
-	
+
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-	
+
 }
--- src/main/java/consul/med/api/infra/security/SecurityFilter.java
+++ src/main/java/consul/med/api/infra/security/SecurityFilter.java
@@ -3,9 +3,12 @@ package consul.med.api.infra.security;
 import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import consul.med.api.domain.usuario.UsuarioRepository;
 import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -18,13 +21,21 @@ public class SecurityFilter extends OncePerRequestFilter {
 	@Autowired
 	private TokenService tokenService;
 	
+	@Autowired
+	private UsuarioRepository repository;
+	
 
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException, ServletException {
 		var tokenJWT = recuperarToken(request);
-		var subject = tokenService.getSubject(tokenJWT);
 		
-		
+		if(tokenJWT != null) {
+			var subject = tokenService.getSubject(tokenJWT);
+			var usuario = repository.findByLogin(subject);
+			
+			var authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+			SecurityContextHolder.getContext().setAuthentication(authentication);
+		}
 		
 		filterChain.doFilter(request, response);
 	}
@@ -32,10 +43,10 @@ public class SecurityFilter extends OncePerRequestFilter {
 
 	private String recuperarToken(HttpServletRequest request) {
 		var authorizationHeader = request.getHeader("Authorization");
-		if (authorizationHeader == null) {
-			throw new RuntimeException("token JWT nao enviado no cabeçlho Authorization");
+		if (authorizationHeader != null) {
+			return authorizationHeader.replace("Bearer ", "");
 		}
-		return authorizationHeader.replace("Bearer ", "");
+		
+		return null;
 	}
-
 }
