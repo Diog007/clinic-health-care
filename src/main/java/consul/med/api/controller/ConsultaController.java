91b0085f8455c7697b5c7ed0b5e61fbe7ec331fd
Diog007 <diogo.mz123@gmail.com>
2024-01-28 17:14:20 -0300

    Implement saving a query to the database

--- src/main/java/consul/med/api/controller/ConsultaController.java
+++ src/main/java/consul/med/api/controller/ConsultaController.java
@@ -1,11 +1,13 @@
 package consul.med.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import consul.med.api.domain.consulta.AgendaDeConsulta;
 import consul.med.api.domain.consulta.DadosAgendamentoConsulta;
 import consul.med.api.domain.consulta.DadosDetalhamentoConsulta;
 import jakarta.transaction.Transactional;
@@ -14,11 +16,14 @@ import jakarta.validation.Valid;
 @RestController
 @RequestMapping("consultas")
 public class ConsultaController {
+	
+	@Autowired
+	private AgendaDeConsulta agenda;
 
 	@PostMapping
 	@Transactional
 	public ResponseEntity agendarConsulta(@RequestBody @Valid DadosAgendamentoConsulta dados) {
-		System.out.println(dados);
+		agenda.agendar(dados);
 		return ResponseEntity.ok(new DadosDetalhamentoConsulta(null, null, null, null));
 	}
 }
---
+++ src/main/java/consul/med/api/domain/consulta/AgendaDeConsulta.java
@@ -0,0 +1,27 @@
+package consul.med.api.domain.consulta;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import consul.med.api.domain.medico.MedicoRepository;
+import consul.med.api.domain.paciente.PacienteRepository;
+
+@Service
+public class AgendaDeConsulta {
+	
+	@Autowired
+	private ConsultaRepository consultaRepository;
+	@Autowired
+	private MedicoRepository medicoRepository;
+	@Autowired
+	private PacienteRepository pacienteRepository;
+	
+	
+	public void agendar(DadosAgendamentoConsulta dados) {
+		
+		var paciente = pacienteRepository.findById(dados.idPaciente()).get();
+		var medico = medicoRepository.findById(dados.idMedico()).get();
+		var consulta = new Consulta(null, medico, paciente, dados.data());
+		consultaRepository.save(consulta);
+	}
+}
