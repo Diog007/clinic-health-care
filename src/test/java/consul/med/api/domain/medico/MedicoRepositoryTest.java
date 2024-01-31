5d81efa616bb3d81f73e097d5103238237c9ced5
Diog007 <diogo.mz123@gmail.com>
2024-01-31 16:42:11 -0300

    Configured automated tests with in-memory database

---
+++ src/test/java/consul/med/api/domain/medico/MedicoRepositoryTest.java
@@ -0,0 +1,20 @@
+package consul.med.api.domain.medico;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
+class MedicoRepositoryTest {
+
+	
+	@Test
+	void escolherMedicoAleatorioLivreNaData() {
+		
+	}
+}
